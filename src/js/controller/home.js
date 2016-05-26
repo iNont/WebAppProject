6bc929b062f282729d8cddbe37cdf37b98f1ddb6
Natchanon Charoensuk <inont@hotmail.co.th>
2016-05-26 23:06:22 +0700

    Create home and controller

---
+++ src/js/controller/home.js
@@ -0,0 +1,8 @@
+angular.module('todoApp', ['ui.router'])
+  .controller('HomeController', function () {
+    self = this
+    self.userInput = ""
+    self.submit = function() {
+      
+    }
+  })
