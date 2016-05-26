9882737e2a2fffa887076a9a847fa88e0d907735
Natchanon Charoensuk <inont@hotmail.co.th>
2016-05-26 23:29:43 +0700

    get and send request to empty url

--- src/js/controller/home.js
+++ src/js/controller/home.js
@@ -1,8 +1,17 @@
 angular.module('todoApp', ['ui.router'])
-  .controller('HomeController', function () {
+  .controller('HomeController', function ($http) {
     self = this
     self.userInput = ""
+    self.result = []
     self.submit = function() {
-      
+      self.result = []
+      $http({
+        method: 'GET',
+        url: 'http://url'
+      }).then(function successCallback(response) {
+        self.result = response.result
+      }, function errorCallback(response) {
+        console.log("Error")
+      });
     }
   })
